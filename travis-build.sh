a1629df94c4631c1518a03534cea0ec6676ce7b8
Dino Omanovic <d.omanovic@tarent.de>
2018-03-27 17:13:44 +0200

    fix broken shellscript...

--- travis-build.sh
+++ travis-build.sh
@@ -4,5 +4,6 @@ docker build -t $TRAVIS_COMMIT .
 docker tag $TRAVIS_COMMIT $DOCKER_USER/blog:$TRAVIS_BRANCH.$TRAVIS_COMMIT
 docker push $DOCKER_USER/blog:$TRAVIS_BRANCH.$TRAVIS_COMMIT
 if [[ $TRAVIS_BRANCH == 'master' ]]
+then
   docker push $DOCKER_USER/blog:latest
 fi
