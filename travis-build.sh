bb19755aae162680fbbe53393959e430acd7d8c9
Dino Omanovic <d.omanovic@tarent.de>
2018-03-27 17:09:41 +0200

    add conditional latest tagging for travis ci build

---
+++ travis-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
+docker build -t $TRAVIS_COMMIT .
+docker tag $TRAVIS_COMMIT $DOCKER_USER/blog:$TRAVIS_BRANCH.$TRAVIS_COMMIT
+docker push $DOCKER_USER/blog:$TRAVIS_BRANCH.$TRAVIS_COMMIT
+if [[ $TRAVIS_BRANCH == 'master' ]]
+  docker push $DOCKER_USER/blog:latest
+fi
